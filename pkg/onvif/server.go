4483e33671d9f3b9ceb454bb2f398d4caea9ecbc
Aleks130699 <52662761+Aleks130699@users.noreply.github.com>
2025-02-01 16:41:17 +0700

    Update server.go

--- pkg/onvif/server.go
+++ pkg/onvif/server.go
@@ -145,7 +145,7 @@ func GetProfilesResponse(names []string) []byte {
 	e.Append(`<trt:GetProfilesResponse>
 `)
 	for _, name := range names {
-		appendProfile(e, "Profiles", name"Profile_000")
+		appendProfile(e, "Profiles", name + "Profile_000")
 	}
 	e.Append(`</trt:GetProfilesResponse>`)
 	return e.Bytes()
@@ -155,7 +155,7 @@ func GetProfileResponse(name string) []byte {
 	e := NewEnvelope()
 	e.Append(`<trt:GetProfileResponse>
 `)
-	appendProfile(e, "Profile", name"Profile_000")
+	appendProfile(e, "Profile", name + "Profile_000")
 	e.Append(`</trt:GetProfileResponse>`)
 	return e.Bytes()
 }
